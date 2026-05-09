4cc0c6042c67e6d1233e7915e9efa992ff132ecb
Sardorbek Pulatov <sardo@hey.com>
2026-05-09 14:06:25 +0200

    cr fixes

--- tools/osspec/internal/verify/verify.go
+++ tools/osspec/internal/verify/verify.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/open-sspm/open-sspm-spec/tools/osspec/internal/celengine"
 	"github.com/open-sspm/open-sspm-spec/tools/osspec/internal/compiler"
+	"github.com/open-sspm/open-sspm-spec/tools/osspec/internal/entitypolicycel"
 	"github.com/open-sspm/open-sspm-spec/tools/osspec/internal/types"
 	"github.com/open-sspm/open-sspm-spec/tools/osspec/internal/yamlstrict"
 )
@@ -123,7 +124,7 @@ func evaluateEntityPolicyPack(pack *types.EntityPolicyPack, input map[string]any
 	if pack == nil {
 		return entityPolicyResult{}, fmt.Errorf("entity policy pack is nil")
 	}
-	env, err := cel.NewEnv(entityPolicyEnvOptions(pack)...)
+	env, err := cel.NewEnv(entitypolicycel.EnvOptions(pack.Metadata.Domain, pack.Spec.Constants)...)
 	if err != nil {
 		return entityPolicyResult{}, fmt.Errorf("%s: create entity policy CEL env: %w", pack.Metadata.ID, err)
 	}
@@ -328,89 +329,6 @@ func evaluateEntityPolicyBool(env *cel.Env, packID, ruleID, expression string, a
 		return false, fmt.Errorf("%s: %s: evaluate %q: expected bool result, got %T", packID, ruleID, expression, value.Value())
 	}
 	return matched, nil
-}
-
-func entityPolicyEnvOptions(pack *types.EntityPolicyPack) []cel.EnvOption {
-	out := entityPolicyDomainEnvOptions(pack.Metadata.Domain)
-	for name := range pack.Spec.Constants {
-		out = append(out, cel.Variable(name, cel.ListType(cel.StringType)))
-	}
-	return out
-}
-
-func entityPolicyDomainEnvOptions(domain types.EntityPolicyDomain) []cel.EnvOption {
-	switch domain {
-	case types.EntityPolicyDomainCredential:
-		return []cel.EnvOption{
-			cel.Variable("source_kind", cel.StringType),
-			cel.Variable("source_name", cel.StringType),
-			cel.Variable("credential_kind", cel.StringType),
-			cel.Variable("status", cel.StringType),
-			cel.Variable("expires_at", cel.NullableType(cel.TimestampType)),
-			cel.Variable("last_used_at", cel.NullableType(cel.TimestampType)),
-			cel.Variable("created_at", cel.NullableType(cel.TimestampType)),
-			cel.Variable("created_by_external_id", cel.StringType),
-			cel.Variable("created_by_display_name", cel.StringType),
-			cel.Variable("approved_by_external_id", cel.StringType),
-			cel.Variable("approved_by_display_name", cel.StringType),
-			cel.Variable("asset_ref_kind", cel.StringType),
-			cel.Variable("asset_ref_external_id", cel.StringType),
-			cel.Variable("scope_json", cel.DynType),
-			cel.Variable("evaluated_at", cel.TimestampType),
-		}
-	case types.EntityPolicyDomainSaaS:
-		return []cel.EnvOption{
-			cel.Variable("canonical_key", cel.StringType),
-			cel.Variable("display_name", cel.StringType),
-			cel.Variable("primary_domain", cel.StringType),
-			cel.Variable("vendor_name", cel.StringType),
-			cel.Variable("source_kind", cel.StringType),
-			cel.Variable("source_name", cel.StringType),
-			cel.Variable("category", cel.StringType),
-			cel.Variable("actors_30d", cel.IntType),
-			cel.Variable("has_privileged_scope", cel.BoolType),
-			cel.Variable("has_confidential_scope", cel.BoolType),
-			cel.Variable("managed_state", cel.StringType),
-			cel.Variable("managed_reason", cel.StringType),
-			cel.Variable("owner_identity_id", cel.IntType),
-			cel.Variable("governance_state", cel.StringType),
-			cel.Variable("review_disposition", cel.StringType),
-			cel.Variable("follow_up_due_date", cel.NullableType(cel.TimestampType)),
-			cel.Variable("configured_business_criticality", cel.StringType),
-			cel.Variable("configured_data_classification", cel.StringType),
-			cel.Variable("effective_business_criticality", cel.StringType),
-			cel.Variable("effective_data_classification", cel.StringType),
-			cel.Variable("connector_binding_configured", cel.BoolType),
-			cel.Variable("connector_binding_enabled", cel.BoolType),
-			cel.Variable("connector_binding_stale", cel.BoolType),
-			cel.Variable("connector_binding_healthy", cel.BoolType),
-			cel.Variable("score", cel.IntType),
-		}
-	case types.EntityPolicyDomainIdentity:
-		return []cel.EnvOption{
-			cel.Variable("identity_id", cel.IntType),
-			cel.Variable("principal_ref", cel.StringType),
-			cel.Variable("principal_type", cel.StringType),
-			cel.Variable("source_kind", cel.StringType),
-			cel.Variable("source_name", cel.StringType),
-			cel.Variable("display_name", cel.StringType),
-			cel.Variable("primary_email", cel.StringType),
-			cel.Variable("last_seen_at", cel.NullableType(cel.TimestampType)),
-			cel.Variable("owner_presence", cel.StringType),
-			cel.Variable("governance_state", cel.StringType),
-			cel.Variable("linked_assets_count", cel.IntType),
-			cel.Variable("linked_credentials_count", cel.IntType),
-			cel.Variable("credential_signals", cel.ListType(cel.StringType)),
-			cel.Variable("has_critical_credential", cel.BoolType),
-			cel.Variable("has_high_risk_credential", cel.BoolType),
-			cel.Variable("has_expired_credential", cel.BoolType),
-			cel.Variable("has_expiring_credential", cel.BoolType),
-			cel.Variable("has_unused_credential", cel.BoolType),
-			cel.Variable("has_stale_evidence", cel.BoolType),
-		}
-	default:
-		return nil
-	}
 }
 
 func normalizeEntityPolicyInput(input map[string]any, domain types.EntityPolicyDomain) (map[string]any, error) {
